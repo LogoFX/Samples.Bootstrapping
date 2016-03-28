225df23d601f2845f45cf126ee4aab2f203e0ac7
Dmitry Bublik <dmitry.buba@gmail.com>
2016-03-28 23:19:02 +0300

    Added second sample

---
+++ Samples.Bootstrapping.SimpleBootstrapper/AppBootstrapper.cs
@@ -0,0 +1,23 @@
+﻿using LogoFX.Client.Bootstrapping;
+using LogoFX.Client.Bootstrapping.Adapters.SimpleContainer;
+using Samples.Bootstrapping.SimpleBootstrapper.ViewModels;
+
+namespace Samples.Bootstrapping.SimpleBootstrapper
+{
+    public sealed class AppBootstrapper : LogoFX.Client.Bootstrapping.SimpleContainer.SimpleBootstrapper<ShellViewModel>
+    {
+        public AppBootstrapper()
+            : base(
+                new ExtendedSimpleContainerAdapter(),
+                new BootstrapperCreationOptions
+                {
+                    DiscoverCompositionModules = true,
+                    InspectAssemblies = true,
+                    UseApplication = true,
+                    ReuseCompositionInformation = true
+                })
+
+        {
+        }
+    }
+}
---
+++ Samples.Bootstrapping.SimpleBootstrapper/ViewModels/ShellViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Caliburn.Micro;
+using JetBrains.Annotations;
+
+namespace Samples.Bootstrapping.SimpleBootstrapper.ViewModels
+{
+    [UsedImplicitly]
+    public sealed class ShellViewModel : Screen
+    {
+        public override string DisplayName
+        {
+            get { return "Simple Bootstrapper Shell"; }
+            set { }
+        }
+    }
+}
