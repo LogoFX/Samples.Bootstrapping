5aa3dbdbf0e9c4e9523b33ba5bfd5fee95851eef
Dmitry Bublik <dmitry.buba@gmail.com>
2016-03-28 22:05:48 +0300

    First sample

---
+++ Samples.Bootstrapping.BootstrapperAsApp/AppBootstrapper.cs
@@ -0,0 +1,34 @@
+﻿using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml.Controls;
+using Caliburn.Micro;
+using LogoFX.Client.Bootstrapping;
+using LogoFX.Client.Bootstrapping.Adapters.WinRTContainer;
+using Samples.Bootstrapping.BootstrapperAsApp.ViewModels;
+
+namespace Samples.Bootstrapping.BootstrapperAsApp
+{
+    public class AppBootstrapper : BootstrapperContainerBase<ShellViewModel, WinRTContainerAdapter, WinRTContainer>
+    {
+        private static readonly WinRTContainer _iocContainer = new WinRTContainer();
+
+        public AppBootstrapper() : base(_iocContainer, c => new WinRTContainerAdapter(c))
+        {
+        }
+
+        protected override void BeforeOnLaunched(LaunchActivatedEventArgs e)
+        {
+            System.Windows.Threading.Dispatch.Current.InitializeDispatch();
+#if DEBUG
+            //if (Debugger.IsAttached)
+            //{
+            //    DebugSettings.EnableFrameRateCounter = true;
+            //}
+#endif  
+        }
+
+        protected override void PrepareViewFirst(Frame rootFrame)
+        {
+            _iocContainer.RegisterNavigationService(rootFrame);
+        }
+    }
+}
---
+++ Samples.Bootstrapping.BootstrapperAsApp/ViewModels/ShellViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Caliburn.Micro;
+using JetBrains.Annotations;
+
+namespace Samples.Bootstrapping.BootstrapperAsApp.ViewModels
+{
+    [UsedImplicitly]
+    public sealed class ShellViewModel : Screen
+    {
+        public override string DisplayName
+        {
+            get { return "Bootstrapper As App Shell"; }
+            set { }
+        }
+    }
+}
