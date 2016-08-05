38f5e8ce54f3d8bc0539b05623a6a54467d779dd
godrose <godrose@gmail.com>
2016-08-05 11:41:39 +0000

    Updated packages.

--- LogoFX.Client.Bootstrapping.Samples.WPF/AppBootstrapper.cs
+++ LogoFX.Client.Bootstrapping.Samples.WPF/AppBootstrapper.cs
@@ -3,19 +3,12 @@ using LogoFX.Client.Bootstrapping.Samples.WPF.ViewModels;
 
 namespace LogoFX.Client.Bootstrapping.Samples.WPF
 {
-    public class AppBootstrapper : BootstrapperContainerBase<ShellViewModel, ExtendedSimpleContainerAdapter>
+    public class AppBootstrapper : BootstrapperContainerBase<ExtendedSimpleContainerAdapter>.WithRootObject<ShellViewModel>
     {
         public AppBootstrapper()
-            : base(new ExtendedSimpleContainerAdapter(),
-                  new BootstrapperCreationOptions
-                  {
-                      DiscoverCompositionModules = true,
-                      InspectAssemblies = true,
-                      ReuseCompositionInformation = true,
-                      UseApplication = true,
-                      UseDefaultMiddlewares = true
-                  })
-        {
-        }
+		:base (new ExtendedSimpleContainerAdapter())
+		{
+			
+		}
     }
 }
